1598d30d550f9a1807f4a7bbe9b31f25d5235479
ishtvankomka <ishtvan.komonii.it.2021@lpnu.ua>
2023-11-19 20:18:04 +0200

    Feat: create order

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -1,9 +1,13 @@
 "use client"
 import useCart from '@/context/CartContext'
+import createOrder from './requests'
+import useAuth from '@/context/AuthContext'
+import { MouseEvent } from 'react'
 
 export default function Page() {
     //const { products, loadingProducts } = useProducts()
     const { cartItems, setCartItems } = useCart()
+    const { worker, customer } = useAuth()
 
     const deleteCartItem = (i: number) => {
         setCartItems([...cartItems]?.filter((e, index) => index !== i))
@@ -36,33 +40,54 @@ export default function Page() {
         setCartItems(newCartItems.filter((e) => e.count))
     }
 
+    const handleOrderProducts = async (e: MouseEvent) => {
+        e.preventDefault()
+        const { email } = customer
+        const data = {
+            items: cartItems,
+            email
+        }
+        await createOrder(data)
+    }
+
     return (
         <main>
             <div>
                 <h1>Cart</h1>
-                <div>
-                    {
-                        cartItems?.map((cartItem, i) => {
-                            return (
-                                <div key={i}>
-                                    <p>{cartItem.name} </p>
-                                    <div>
-                                        <button onClick={() => decreaseCartItemCount(i)}>
-                                            -
-                                        </button>
-                                        <b>{cartItem.count}</b>
-                                        <button onClick={() => inreaseCartItemCount(i)}>
-                                            +
-                                        </button>
-                                    </div>
-                                    <button onClick={() => deleteCartItem(i)}>
-                                        delete
-                                    </button>
-                                </div>
-                            )
-                        })
-                    }
-                </div>
+                {
+                    cartItems.length ?
+                        <div>
+                            <div>
+                                {
+                                    cartItems?.map((cartItem, i) => {
+                                        return (
+                                            <div key={i}>
+                                                <p>{cartItem.name} </p>
+                                                <div>
+                                                    <button onClick={() => decreaseCartItemCount(i)}>
+                                                        -
+                                                    </button>
+                                                    <b>{cartItem.count}</b>
+                                                    <button onClick={() => inreaseCartItemCount(i)}>
+                                                        +
+                                                    </button>
+                                                </div>
+                                                <button onClick={() => deleteCartItem(i)}>
+                                                    delete
+                                                </button>
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </div>
+                            <div>
+                                <button onClick={(e) => handleOrderProducts(e)}>
+                                    Order products
+                                </button>
+                            </div>
+                        </div>
+                        : null
+                }
             </div>
         </main>
     )
---
+++ pages/cart/requests.ts
@@ -0,0 +1,30 @@
+import { db } from "@/firebase/config";
+import { CartItem } from "@/types/CartProduct";
+import { addDoc, collection } from "firebase/firestore";
+
+type OrderData = {
+    items: CartItem[]
+    email: string
+}
+
+export default async function createOrder(data: OrderData) {
+    const {
+        items,
+        email
+    } = data
+
+    let result = null,
+        error = null;
+    try {
+        result = await addDoc(collection(db, "Orders"), {
+            date_created: new Date().getTime(),
+            items,
+            email
+        });
+
+    } catch (e) {
+        error = e;
+    }
+
+    return { result, error };
+}
